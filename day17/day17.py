d1215e402fa256c1d653e18c00dffaa979ad701f
JaneSoboleva <89308253+JaneSoboleva@users.noreply.github.com>
2023-12-17 23:38:44 +0200

    Add files via upload
    ...actually nevermind, I rather quickly realized the problem and fixed it. Phew.

--- day17/day17.py
+++ day17/day17.py
@@ -2,11 +2,6 @@ with open("day17_input.txt", 'r') as day17_file:
     heatloss = [[int(ch) for ch in line] for line in day17_file.read().splitlines()]
 
 maximum_steps_in_one_direction = 10
-minimums = [[{"up": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
-              "down": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
-              "left": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
-              "right": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)}} for i in range(len(heatloss[0]))] for j in range(len(heatloss))]
-# basically, minimums[row][column][direction, after taking which we ended up here][steps so far in this direction] = minimum heat loss found so far
 clockwise_change = {"up": "right", "right": "down", "down": "left", "left": "up"}
 counterclockwise_change = {"up": "left", "left": "down", "down": "right", "right": "up"}
 direction_movements = {"right": [0, 1], "left": [0, -1], "up": [-1, 0], "down": [1, 0]}
@@ -32,28 +27,28 @@ def turn_left(value):
     value[3] = 0
 def attempt_movement(item, direction, phase):
     new_item = item.copy()
-    # special restrictions for phases 1 and 2 ---
+
     if direction == "straight":
-        if phase == 1:
-            if new_item[3] >= 3:
-                return  # phase 1, can't go straight for more than 3 blocks
-        if phase == 2:
-            if new_item[3] >= 10:
-                return  # phase 2, can't go straight for more than 10 blocks
+        if phase == 1 and new_item[3] >= 3:
+            return  # phase 1, can't go straight for more than 3 blocks
+        if phase == 2 and new_item[3] >= 10:
+            return  # phase 2, can't go straight for more than 10 blocks
     if direction == "right":
-        if phase == 2:
-            if new_item[3] < 4:
-                return  # phase 2, can't turn right if went straight for less than 4 blocks
         turn_right(new_item)
     if direction == "left":
-        if phase == 2:
-            if new_item[3] < 4:
-                return  # phase 2, can't turn left if went straight for less than 4 blocks
         turn_left(new_item)
-    # --- end of specifying restrictions
-    get_moving(new_item)
+
+    new_minimum = get_minimum(item)
+    while True:
+        get_moving(new_item)
+        if inbounds(new_item):
+            new_minimum += get_heatloss(new_item)
+        if phase == 1:
+            break  # in phase 1 we need to run this only once, since we can make turns at any point
+        if phase == 2 and new_item[3] >= 4:
+            break  # in phase 2 we should move for at least 4 blocks straight before accounting for a new minimum
+            # and it's important to not update minimums on the previous 3 blocks, otherwise it's not different from being able to make a turn early, which leads to a wrong answer
     if inbounds(new_item):
-        new_minimum = get_minimum(item) + get_heatloss(new_item)
         if get_minimum(new_item) > new_minimum:
             update_minimum(new_item, new_minimum)
             newly_updated.append(new_item)
@@ -65,6 +60,11 @@ def get_minimum_by_coordinates(row, col):
     return rslt
 
 
+minimums = [[{"up": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "down": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "left": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "right": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)}} for i in range(len(heatloss[0]))] for j in range(len(heatloss))]
+# basically, minimums[row][column][direction, after taking which we ended up here][steps so far in this direction] = minimum heat loss found so far
 minimums[0][0]["right"][0] = 0
 recently_updated = [[0, 0, "right", 0]]  # row, column, direction, steps so far
 while len(recently_updated) > 0:
@@ -76,11 +76,12 @@ while len(recently_updated) > 0:
     recently_updated = [x[:] for x in newly_updated]
 print("Phase 01 result:", get_minimum_by_coordinates(-1, -1))
 
-# copypasting, too lazy to do this more efficiently
-minimums = [[{"up": {i: 10**9 for i in range(11)},
-              "down": {i: 10**9 for i in range(11)},
-              "left": {i: 10**9 for i in range(11)},
-              "right": {i: 10**9 for i in range(11)}} for i in range(len(heatloss[0]))] for j in range(len(heatloss))]
+# copypasting, too lazy to do this more efficiently... the only change is phase=1 => phase=2
+minimums = [[{"up": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "down": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "left": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)},
+              "right": {i: 10**9 for i in range(maximum_steps_in_one_direction + 1)}} for i in range(len(heatloss[0]))] for j in range(len(heatloss))]
+# basically, minimums[row][column][direction, after taking which we ended up here][steps so far in this direction] = minimum heat loss found so far
 minimums[0][0]["right"][0] = 0
 recently_updated = [[0, 0, "right", 0]]  # row, column, direction, steps so far
 while len(recently_updated) > 0:
@@ -90,10 +91,4 @@ while len(recently_updated) > 0:
         attempt_movement(r_item, "right", phase=2)
         attempt_movement(r_item, "left", phase=2)
     recently_updated = [x[:] for x in newly_updated]
-# tamper with the minimums so that we only account for those that had >= 4 movements in the same direction prior to arriving to the destination
-# to comply with this: "...before it can turn (or even before it can stop at the end)..."
-for d in direction_movements:
-    for i in range(4):
-        minimums[-1][-1][d][i] = 10 ** 9
-# ---
 print("Phase 02 result:", get_minimum_by_coordinates(-1, -1))
